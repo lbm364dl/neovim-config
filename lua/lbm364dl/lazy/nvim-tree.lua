59088234b2e339a6539b1a27140970bdd167b77c
lbm364dl <ctl.covaci@gmail.com>
2025-08-21 10:55:04 +0200

    Open file with l

--- lua/lbm364dl/lazy/nvim-tree.lua
+++ lua/lbm364dl/lazy/nvim-tree.lua
@@ -6,7 +6,20 @@ return {
   dependencies = {
     "nvim-tree/nvim-web-devicons",
   },
+  config = function()
+    require("nvim-tree").setup({
+      on_attach = function(bufnr)
+      	local api = require("nvim-tree.api")
+
+      	-- default mappings
+      	api.config.mappings.default_on_attach(bufnr)
+
+      	-- custom mappings (buffer-local!)
+      	vim.keymap.set("n", "l", api.node.open.edit, {desc = "nvim-tree: Open file"})
+			end
+    })
+  end,
   keys = {
-      { "<leader>e", "<cmd>NvimTreeToggle<cr>", desc = "Toggle file explorer" }
+      { "<leader>e", "<cmd>NvimTreeToggle<cr>", desc = "Toggle file explorer" },
   }
 }
