ab659a1505b42cf7eab017b08417f581383aa4d8
lbm364dl <ctl.covaci@gmail.com>
2025-08-21 22:20:54 +0200

    Ask if save for unsaved changed buffer before closing

--- lua/lbm364dl/mappings.lua
+++ lua/lbm364dl/mappings.lua
@@ -1,6 +1,21 @@
 vim.keymap.set("n", "<C-s>", "<cmd>w<CR>", { desc = "Save file" })
 vim.keymap.set("i", "hj", "<Esc>", { desc = "Leave insert mode" })
-vim.keymap.set("n", "<leader>c", ":bdelete<CR>", { desc = "Close current buffer " })
+
+vim.keymap.set("n", "<leader>c", function()
+	if vim.bo.modified then
+		local choice = vim.fn.confirm("Buffer has unsaved changes. Save?", "&Yes\n&No\n&Cancel", 1)
+		if choice == 1 then
+			vim.cmd("write")
+			vim.cmd("bdelete")
+		elseif choice == 2 then
+			vim.cmd("bdelete!")
+		else
+			return
+		end
+	else
+		vim.cmd("bdelete")
+	end
+end, { desc = "Close current buffer" })
 
 vim.keymap.set("n", "<leader>lf", function()
 	vim.lsp.buf.format({ async = true }) -- async formatting
