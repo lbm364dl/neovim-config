f9f98e17f7c913b0d3643a2da7bd20b5b612d7cb
lbm364dl <ctl.covaci@gmail.com>
2025-08-21 10:55:33 +0200

    Add lua_ls LSP

--- lua/lbm364dl/lazy/mason-lspconfig.lua
+++ lua/lbm364dl/lazy/mason-lspconfig.lua
@@ -1,7 +1,7 @@
 return {
     "mason-org/mason-lspconfig.nvim",
     opts = {
-        ensure_installed = { "r_language_server", "air", "ruff", "pyright" },
+        ensure_installed = { "r_language_server", "air", "ruff", "pyright", "lua_ls" },
     },
     dependencies = {
         { "mason-org/mason.nvim", opts = {} },
