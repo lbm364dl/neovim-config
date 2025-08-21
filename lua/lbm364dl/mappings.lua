8c1894905e37376e901e51e2b508dfd3411278a5
lbm364dl <ctl.covaci@gmail.com>
2025-08-21 11:56:47 +0200

    Add open diagnostic mapping

--- lua/lbm364dl/mappings.lua
+++ lua/lbm364dl/mappings.lua
@@ -2,6 +2,9 @@ vim.keymap.set("n", "<C-s>", "<cmd>w<CR>", { desc = "Save file" })
 vim.keymap.set("i", "hj", "<Esc>", { desc = "Leave insert mode" })
 vim.keymap.set("n", "<leader>c", ":bdelete<CR>", { desc = "Close current buffer " })
 
-vim.keymap.set("n", "<leader>f", function()
+vim.keymap.set("n", "<leader>lf", function()
 	vim.lsp.buf.format({ async = true }) -- async formatting
 end, { desc = "Format current buffer" })
+
+-- If diagnostics floating window is too large, you can go inside with <C-w> w after opening it
+vim.keymap.set("n", "<leader>ld", vim.diagnostic.open_float, { desc = "Show diagnostic" })
