0b3c82321bba769341d9a718c5cfef4ffd89c2c5
lbm364dl <ctl.covaci@gmail.com>
2025-08-21 10:55:16 +0200

    Add close buffer mapping

--- lua/lbm364dl/mappings.lua
+++ lua/lbm364dl/mappings.lua
@@ -1,2 +1,4 @@
 vim.keymap.set("n", "<C-s>", "<cmd>w<CR>", { desc = "Save file" })
 vim.keymap.set("i", "hj", "<Esc>", { desc = "Leave insert mode" })
+vim.keymap.set("n", "<leader>c", ":bdelete<CR>", { desc = "Close current buffer "})
+
