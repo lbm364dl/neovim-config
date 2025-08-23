701dfc7c64229a793bd43b8621c7e0a85755b261
lbm364dl <ctl.covaci@gmail.com>
2025-08-23 22:00:21 +0200

    Change tabs to spaces

--- lua/lbm364dl/settings.lua
+++ lua/lbm364dl/settings.lua
@@ -1,6 +1,7 @@
 vim.opt.clipboard = "unnamedplus"
 vim.cmd.colorscheme("tokyonight")
 vim.opt.relativenumber = true
+vim.opt.expandtab = true
 
 vim.api.nvim_create_autocmd("FileType", {
 	pattern = { "python" },
