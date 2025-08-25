02fdd85474aa06e45920cd0938818648b71d8651
lbm364dl <ctl.covaci@gmail.com>
2025-08-25 22:41:11 +0200

    Add devtools::load_all() R shortcut

--- lua/lbm364dl/lazy/r-nvim.lua
+++ lua/lbm364dl/lazy/r-nvim.lua
@@ -1,5 +1,14 @@
 return {
-	"R-nvim/R.nvim",
-	-- Only required if you also set defaults.lazy = true
-	lazy = false
+  "R-nvim/R.nvim",
+  -- Only required if you also set defaults.lazy = true
+  lazy = false,
+  keys = {
+    {
+      "<localleader>rl",
+      function() require("r.send").cmd("devtools::load_all()") end,
+      mode = "n",
+      ft = "r",
+      desc = "R devtools::load_all()",
+    },
+  },
 }
