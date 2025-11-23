567bad8087273780ac12f92ede79fcfeef9b671b
lbm364dl <ctl.covaci@gmail.com>
2025-11-23 12:31:45 +0100

    Add CodeCompanion

---
+++ lua/lbm364dl/lazy/codecompanion.lua
@@ -0,0 +1,19 @@
+return {
+  "olimorris/codecompanion.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim"
+  },
+  opts = {},
+  -- opts = {
+  --   strategies = {
+  --     chat = {
+  --       adapter = "copilot",
+  --       model = "gpt-4.1"
+  --     },
+  --   },
+  --   -- NOTE: The log_level is in `opts.opts`
+  --   opts = {
+  --     log_level = "DEBUG",
+  --   },
+  -- },
+}
